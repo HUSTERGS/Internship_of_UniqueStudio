5301ab22ed9e26544577aea90d3e2f22bc04668b
HUSTGS <1148690954@qq.com>
2018-05-12 19:37:03 +0800

    基本完成，准备精简一下CSS

--- 4th/index/js/js.js
+++ 4th/index/js/js.js
@@ -17,11 +17,21 @@ document.querySelector(".main-page").addEventListener("pointermove", function(ev
 
 var container = document.querySelector(".content-container");
 var currentpic = 1;
+var currentpage = 1;
 var changebtnleft = document.querySelector(".left-btn img");
 var changebtnright = document.querySelector(".right-btn img");
+var topbtn = document.querySelector(".top-tag img");
+var kindlebtn = document.querySelector(".kindle-tag img");
+var mainbtn = document.querySelector(".main-tag img");
+var searchbtn = document.querySelector(".search-tag img");
+
 
 changebtnleft.addEventListener("pointerdown", function(){
     this.src = "../切图/Banner2/01/btn_leftclick.png";
+})
+
+changebtnright.addEventListener("pointerdown", function(){
+    this.src = "../切图/Banner2/01/btn_rightclick.png";
 })
 
 changebtnleft.addEventListener("pointerup", function(){
@@ -37,9 +47,6 @@ changebtnleft.addEventListener("pointerup", function(){
     }
 })
 
-changebtnright.addEventListener("pointerdown", function(){
-    this.src = "../切图/Banner2/01/btn_rightclick.png";
-})
 
 changebtnright.addEventListener("pointerup", function(){
     this.src = "../切图/Banner2/01/btn_right.png";
@@ -54,7 +61,80 @@ changebtnright.addEventListener("pointerup", function(){
     }
 })
 
-function slidePic1(){
-    container.style.transform = "translateX(-100%)";
-}
+topbtn.addEventListener("pointerdown", function(){
+    this.src = "../切图/nav/Top选中.png";
+    $("html,body").animate({scrollTop: $("#main").offset().top}, 1000);
+    currentpage = 1;
+})
 
+topbtn.addEventListener("pointerup", function() {
+    this.src = "../切图/nav/Top.png";
+})
+
+kindlebtn.addEventListener("pointerdown", function(){
+    this.src = "../切图/nav/Kindle选中.png";
+    $("html,body").animate({scrollTop: $("#read-feel").offset().top}, 1000);
+    currentpage = 2;
+})
+
+kindlebtn.addEventListener("pointerup", function() {
+    this.src = "../切图/nav/Kindle.png";
+})
+
+mainbtn.addEventListener("pointerdown", function(){
+    this.src = "../切图/nav/Main选中.png";
+    $("html,body").animate({scrollTop: $("#advantages").offset().top}, 1000);
+    currentpage = 3;
+})
+
+mainbtn.addEventListener("pointerup", function() {
+    this.src = "../切图/nav/Main.png";
+})
+
+searchbtn.addEventListener("pointerdown", function(){
+    this.src = "../切图/nav/Search选中.png";
+    $("html,body").animate({scrollTop: $("#know-more").offset().top}, 1000);
+    currentpage = 4;
+})
+
+searchbtn.addEventListener("pointerup", function() {
+    this.src = "../切图/nav/Search.png";
+})
+
+
+window.addEventListener("mousewheel", function(event){
+    if (! document.documentElement.scrollTop  === 0) currentpage = 1;
+    if (document.documentElement.scrollTop === window.innerHeight) currentpage = 2;
+    if (document.documentElement.scrollTop === 2 * window.innerHeight) currentpage = 3;
+    if (document.documentElement.scrollTop === 3 * window.innerHeight) currentpage = 4;
+    if (document.documentElement.scrollTop <= 3 * window.innerHeight){
+        if (event.wheelDelta < 0) {
+            if (currentpage == 1){
+                $("html,body").animate({scrollTop: $("#read-feel").offset().top}, 1000);
+                currentpage = 2;
+            }
+            else if (currentpage === 2) {
+                $("html,body").animate({scrollTop: $("#advantages").offset().top}, 1000);
+                currentpage = 3;
+            }
+            else if (currentpage === 3) {
+                $("html,body").animate({scrollTop: $("#know-more").offset().top}, 1000);
+                currentpage = 4;
+            }
+        }
+        else if (event.wheelDelta > 0) {
+            if (currentpage == 2) {
+                $("html,body").animate({scrollTop: $("#main").offset().top}, 1000);
+                currentpage = 1;
+            }
+            else if (currentpage === 3) {
+                $("html,body").animate({scrollTop: $("#read-feel").offset().top}, 1000);
+                currentpage = 2;
+            }
+            else if (currentpage === 4) {
+                $("html,body").animate({scrollTop: $("#advantages").offset().top}, 1000);
+                currentpage = 3;
+            }
+        }
+    }
+})
